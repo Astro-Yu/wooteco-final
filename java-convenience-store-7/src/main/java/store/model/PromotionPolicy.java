ee3b29b9dac5f49a6f8b632c2527a6403af0d635
yush <yush2202@gmail.com>
2024-12-12 22:53:45 +0900

    feat(PromotionPolicy): 프로모션 정책 기능 구현

---
+++ java-convenience-store-7/src/main/java/store/model/PromotionPolicy.java
@@ -0,0 +1,11 @@
+package store.model;
+
+public class PromotionPolicy {
+    private final int buy;
+    private final int get;
+
+    public PromotionPolicy(int buy, int get) {
+        this.buy = buy;
+        this.get = get;
+    }
+}
