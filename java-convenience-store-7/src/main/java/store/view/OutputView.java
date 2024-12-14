875c42039caa6e8c7eaf33bc235f98139302e98c
yush <yush2202@gmail.com>
2024-12-14 12:44:05 +0900

    feat(커밋확인): 커밋확인
    ㅎㅎ

--- java-convenience-store-7/src/main/java/store/view/OutputView.java
+++ java-convenience-store-7/src/main/java/store/view/OutputView.java
@@ -1,7 +1,21 @@
 package store.view;
 
+import java.util.List;
+import store.model.dto.ItemDto;
+
 public class OutputView {
-    public void printItem() {
-        
+    private static final String ITEM_FORMAT = "- %s %,d원 %s개 %s";
+
+    public void printItems(List<ItemDto> itemDtos) {
+        for (ItemDto itemDto : itemDtos) {
+            printItem(itemDto);
+        }
+    }
+
+    private void printItem(ItemDto itemDto) {
+        String message = String.format(ITEM_FORMAT, itemDto.name(), itemDto.price(), itemDto.quantity(),
+                itemDto.promotionName());
+
+        System.out.println(message);
     }
 }
